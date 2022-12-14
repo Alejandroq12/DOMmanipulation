9fb983765104426ad92595dfab90b737618a1a15
Alejandroq12 <quezada.rodriguez6@gmail.com>
2022-12-14 12:26:53 -0600

    updating

--- script.js
+++ script.js
@@ -1,31 +1,81 @@
-
 var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul")
+var deleteBtns = document.getElementsByClassName("delete");
+var items = ul.getElementsByTagName("li");
 
-function inputLength() {
-    return input.value.length;
+
+
+//add event listener to first 6 btns in HTML file
+for(var i = 0; i < deleteBtns.length; i++){
+	deleteBtns[i].addEventListener("click", removeParent, false);
+}
+
+
+//from StackOverflow:
+function removeParent(evt) {
+  evt.target.removeEventListener("click", removeParent, false);
+  evt.target.parentNode.remove();
+}
+
+//click on a list item and it strikethroughs the text
+function lineThrough(event){
+	var a=event.target;
+	
+	if(count==0)
+	{
+		
+	    a.classList.add("done");
+	}
+	else
+	{
+		a.classList.toggle("done");
+	}
+	count++;
+
+
+}
+
+ul.onclick = function(event){
+	var target = getEventTarget(event);
+	target.classList.toggle("done");
+}
+
+
+//adding new items:
+
+function inputLength(){
+	return input.value.length;
 }
 
 function createListElement() {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
+	var btn = document.createElement("button");
+	btn.innerHTML = "Delete";
+	btn.onclick = removeParent;
+
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	li.innerHTML = li.innerHTML + " ";
+	li.appendChild(btn);
+
+	ul.appendChild(li);
+	input.value="";
 }
 
-function addListAfterClick() {
-    if (inputLength() > 0) {
-        createListElement();
-    }
+
+function addToListAfterClick(){
+	if(inputLength() > 0){
+			createListElement();
+		}
 }
 
-function addListAfterKeypress(event) {
-    if (inputLength() > 0 && event.which === 13) {
-        createListElement();
-    }
+function addToListAfterKeypress(event){
+	if(inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
 }
 
-button.addEventListener("click", addListAfterClick);
-    
-input.addEventListener("keypress", addListAfterKeypress);
+
+button.addEventListener("click", addToListAfterClick);
+
+input.addEventListener("keypress", addToListAfterKeypress);
